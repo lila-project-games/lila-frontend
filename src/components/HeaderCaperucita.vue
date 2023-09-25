c0989456873649991f42191d5b04c1596ad4012c
jumelis <jumelisrosales@gmail.com>
2023-09-25 14:17:52 +0200

    headerCaperucita added

---
+++ src/components/HeaderCaperucita.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="../../public/images/HeaderCaperucita.svg" class="img-fluid">
+</template>
--- src/views/Caperucita.vue
+++ src/views/Caperucita.vue
@@ -1,7 +1,8 @@
 <script setup>
-
+import HeaderCaperucita from '../components/HeaderCaperucita.vue';
 </script>
 
 <template>
     <h1>Caperucita</h1>
+    <HeaderCaperucita />
 </template>
