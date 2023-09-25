3ab188699c9ac4f6cd8229c95668080d0cceb162
alexscript <alejandro.93lf@gmail.com>
2023-09-25 11:37:19 +0200

    tabla

---
+++ src/components/CaperucitaTabla.vue
@@ -0,0 +1,134 @@
+<script>
+</script>
+
+
+
+
+<template>
+<div id="container-table">
+<table>
+
+<thead>
+<tr>
+    <th></th>
+    <th class="thcabecera">¿CÓMO ES?</th>
+    <th class="thcabecera">OBJETO QUE LO IDENTIFICA</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td class="imgcap">
+            <img src="../assets/images/caperucita.svg" alt="caperucita">
+        </td>
+        <td>
+            <textarea class="textzone" name="comoCaperucita" rows="3" cols="50" maxlength="145">
+            </textarea> 
+
+        </td>
+        <td>
+            <textarea class="textzone" name="objetoCaperucita" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+    </tr>
+    <tr>
+        <td class="imgcap">
+            <img src="../assets/images/lobo.svg" alt="lobo">
+        </td>
+        <td>
+            <textarea class="textzone" name="comoLobo" rows="3" cols="50" maxlength="145">
+            </textarea>        
+        </td>
+        <td>
+            <textarea class="textzone" name="obejetoLobo" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+    </tr>
+    <tr>
+        <td class="imgcap">
+            <img src="../assets/images/abuelita.svg" alt="abuelita">
+        </td>
+        <td>
+            <textarea class="textzone" name="comoAbuelita" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+        <td>
+            <textarea class="textzone" name="objetoAbuelita" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+    </tr>
+    <tr>
+        <td class="imgcap">
+            <img src="../assets/images/cazador.svg" alt="cazador">
+        </td>
+        <td>
+            <textarea class="textzone" name="comoCazador" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+        <td>
+            <textarea class="textzone" name="objetoCazador" rows="3" cols="50" maxlength="145">
+            </textarea> 
+        </td>
+    </tr>
+
+</tbody>
+
+</table>
+</div>
+
+
+
+</template>
+
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
+
+
+table{
+    
+    font-family: 'Itim', cursive;
+    border: 0px;
+    padding:0; 
+    margin:0;
+    border-collapse: collapse;
+    
+}
+
+textarea{
+    resize:none;
+    overflow:hidden;
+    text-align: center;
+}
+td{
+    border: 2px solid #8c52fe;
+    margin: 0;
+    padding: 0;
+}
+
+#container-table{
+   display: flex;
+   justify-content: center;
+}
+.imgcap{
+   
+    height: 150px;
+    width: 150px;
+}
+
+.thcabecera{
+    font-size: 1.5em;
+    text-align: center;
+    height: 50px;
+    width: 400px;
+}
+.textzone{
+border: 0px;
+ height: 150px;
+ width: 400px;   
+ outline: none;
+ text-align: center;
+ font-size: 1.6em;
+}
+</style>
--- src/views/Caperucita.vue
+++ src/views/Caperucita.vue
@@ -1,7 +1,21 @@
 <script setup>
-
+import CaperucitaTabla from '../components/CaperucitaTabla.vue'
 </script>
 
 <template>
-    <h1>Caperucita</h1>
+    
+    <h1>Caperucita Roja</h1>
+    <CaperucitaTabla class="tabla"/>
+    
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap');
+
+h1{
+    font-size: 2.8em;
+    font-family: 'Luckiest Guy', cursive;
+    text-align: center;
+    color: #FF2F32;
+}
+</style>
